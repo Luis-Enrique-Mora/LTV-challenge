e2c4e0f888e2a26ec02b165ff5e889f5144b7b3a
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2022-05-06 22:13:57 -0600

    search by phone implemeted

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   const proxyurl = "";
   const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
   var emailError = false;
+  var phoneError = false;
   
   // it request to the api and redirects to result page
   function request( urlAndQuerystring ) {
@@ -31,7 +32,7 @@ $(document).ready(function () {
     let emailInput = document.querySelector( 'input[name="email"]' );
     // if there is not error, execute the request, else show error message to the user
     if ( emailError === false ) {
-      emailInput.parentNode.classList.remove( "error" );
+        emailInput.parentNode.classList.remove( "error" );
         let urlAndQuerystring = url + 'email=' + emailValue;
         request( urlAndQuerystring );
 
@@ -40,7 +41,7 @@ $(document).ready(function () {
     }
   });
 
-  // execute logic in case of pressing enter while typing in the input
+  // execute logic in case of pressing enter while typing in the email input
   $( 'input[name="email"]' ).keypress( function ( event ) {
     let emailValue = $( 'input[name="email"]' ).val().toLowerCase();
     let emailInput = document.querySelector( 'input[name="email"]' );
@@ -70,6 +71,65 @@ $(document).ready(function () {
         request( urlAndQuerystring );
       } else if ( emailError !== false ) {
         emailInput.parentNode.classList.add( "error" );
+      }
+    }
+  });
+
+  $( "#btn-search-by-phone" ).on( "click", function ( e ) {
+    e.preventDefault();
+    localStorage.clear(); //Clears storage for next request
+    var phoneValue = parseInt( $( 'input[name="phone"]' ).val() );
+    console.log(phoneValue, typeof(phoneValue));
+    // phone validation
+    phoneValue = typeof( phoneValue ) == 'number' && phoneValue.toString().length == 10 ? phoneValue : false;
+
+    if ( phoneValue ) {
+        phoneError = false;
+    } else {
+        phoneError = true;
+    }
+
+    let phoneInput = document.querySelector( 'input[name="phone"]' );
+    
+    if ( phoneError === false ) {
+      phoneInput.parentNode.classList.remove( "error" );
+      var urlAndQuerystring = url + 'phone=' + phoneValue;
+      // Request to the LTV api and redirect to result page
+      request( urlAndQuerystring );
+    } else if ( phoneError !== false ) {
+        phoneInput.parentNode.classList.add("error");
+    }
+  });
+
+  // execute logic in case of pressing enter while typing in the phone input
+  $( 'input[name="phone"]' ).keypress(function ( event ) {
+
+    let phoneValue = $( 'input[name="phone"]' ).val().toLowerCase();
+    phoneValue = typeof( phoneValue ) == 'number' && phoneValue.toString().length == 10 ? phoneValue : false;
+    
+    if ( phoneValue ) {
+        phoneError = false;
+    } else {
+        phoneError = true;
+    }
+
+    keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {
+      /**
+       * Makes a request to ltv API to search an specific email address.
+       * If there's a response, it gets stored in the local storage and redirects to results page
+       */
+      event.preventDefault();
+      localStorage.clear(); //Clears storage for next request
+
+      let phoneInput = document.querySelector( 'input[name="phone"]' );
+      if ( phoneError === false ) {
+        phoneInput.parentNode.classList.remove( "error" );
+        var urlAndQuerystring = url + 'phone=' + phoneValue;
+        // Request to the LTV api and redirect to result page
+        request( urlAndQuerystring );
+      } else if ( phoneError !== false ) {
+          phoneInput.parentNode.classList.add( "error" );
       }
     }
   });
