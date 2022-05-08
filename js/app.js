446ce6640270686c7294e16d83af65b796322a7e
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2022-05-08 00:12:57 -0600

    search by email when click button implemented

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
-
+  
   // variables
-  var searchType = 'email';
+  var searchType = 'email'; // starts as email by default
+  const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  const proxyurl = "";
+  const url = 'https://ltv-data-api.herokuapp.com/api/v1/records.json?';
   // DOM variables for DOM manipulation
   const _ = {
     searchByEmailBtn : document.querySelector("#btn-search-by-email"),
@@ -9,10 +12,10 @@ $(document).ready(function () {
     input : document.querySelector( ".form-control" ),
     errorMessage : document.querySelector( ".error-msg" )
   };
-
+  
   $( "#btn-search-by-email" ).on( "click", function( e ) {
     e.preventDefault();
-
+    
     _.searchByphoneBtn.classList.remove( "btn-warning" );
     _.searchByphoneBtn.classList.add( "btn-outline-warning" );
     _.searchByEmailBtn.classList.add( "btn-warning" );
@@ -20,13 +23,13 @@ $(document).ready(function () {
     _.errorMessage.innerText = "Please enter a valid email address";
     _.input.placeholder = "ENTER AN EMAIL ADDRESS";
     _.errorMessage.parentNode.classList.remove( "error" );
-
+    
     searchType = 'email';
   });
-
+  
   $( "#btn-search-by-phone").on( "click", function( e ) {
     e.preventDefault();
-
+    
     _.searchByphoneBtn.classList.add( "btn-warning" );
     _.searchByphoneBtn.classList.remove( "btn-outline-warning" );
     _.searchByEmailBtn.classList.remove( "btn-warning" );
@@ -34,38 +37,47 @@ $(document).ready(function () {
     _.errorMessage.innerText = "Please enter a valid phone number";
     _.input.placeholder = "ENTER A PHONE NUMBER";
     _.errorMessage.parentNode.classList.remove( "error" );
-
+    
     searchType = 'phone';
   });
+  
+  function redirectToResultPage(){
+    window.location.href = "result.html";
+  }
+  
+  function request( urlPlusQueryString ) {
+    fetch(proxyurl + urlPlusQueryString)
+      .then((response) => response.text())
+      .then(function (contents) {
+        localStorage.setItem("userObject", contents);
+        redirectToResultPage();
+      })
+      .catch((e) => console.log(e));
+  }
+    
 
   $("#btn-search").on("click", function (e) {
+
     e.preventDefault();
     localStorage.clear(); //Clears storage for next request
-    email = $('input[type="text"]').val().toLowerCase();
-
-    var x, y;
-    regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    if (email.match(regEx)) {
-      x = true;
+    var inputValue = _.input.value.toLowerCase();
+    console.log( _.input );
+    console.log( typeof(inputValue), inputValue );
+    // Selecting type of search, if type of search email: email request, else: phone request
+    if( searchType == "email" ) {
+      // If email is valid proceed with the request, else show an error message
+      if ( inputValue.match( emailRegEx ) ) {
+        _.errorMessage.parentNode.classList.remove( "error" );
+        const urlPlusQueryString = url + 'email=' + inputValue;
+        // it makes a request to the api with an email address and redirects to page result
+        request( urlPlusQueryString );
+      } else {
+        _.errorMessage.parentNode.classList.add( "error" );
+      }
     } else {
-      x = false;
+      
     }
 
-    if (x === true) {
-      document.querySelector('input[type="text"]').parentNode.classList.remove("error");
-      const proxyurl = "";
-      const url =
-        'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
-      fetch(proxyurl + url)
-        .then((response) => response.text())
-        .then(function (contents) {
-          localStorage.setItem("userObject", contents);
-          window.location.href = "result.html";
-        })
-        .catch((e) => console.log(e));
-    } else if (x !== true) {
-      document.querySelector('input[type="text"]').parentNode.classList.add("error");
-    }
   });
 
   $('input[type="text"]').keypress(function (event) {
